50e2357e72a14d52fbf09d9d2df6b4a47fa657b9
Jacob Torrey <discipleofranok@gmail.com>
2014-01-12 16:20:26 -0700

    Ensured there is a DB link prior to calling mysql_real_escape_string()
    Signed-off-by: Jacob Torrey <discipleofranok@gmail.com>

--- addsong.php
+++ addsong.php
@@ -4,13 +4,15 @@ if ($_POST['name'] == "" || $_POST['track'] == "" || $_POST['artist'] == "" || $
 {
 	die("Not all values filled in!");
 }
+
+$db = new DB();
+
 $name = mysql_real_escape_string($_POST['name']);
 $artist = mysql_real_escape_string($_POST['artist']);
 $track = mysql_real_escape_string($_POST['track']);
 $link = mysql_real_escape_string($_POST['link']);
 $time = time();
 
-$db = new DB();
 $db->query("INSERT INTO `songshare` (`name`, `track`, `artist`, `link`, `date`) VALUES ('$name', '$track', '$artist', '$link', '$time');");
 
 header('Location: index.php');
